65a7af45100317f8436df3144e714039d563748d
Fülöp Tibor <fuloptibi03@gmail.com>
2022-07-04 02:52:59 +0300

    Massive Rewrite

--- userscript/unclutter.js
+++ userscript/unclutter.js
@@ -33,26 +33,90 @@
         return h >>> 0
     }
 
-    let waitForButtonBar = null;
-
-    let location = window.location.href;
-    if (location.match(/^https:\/\/www\.youtube\.com\/watch\?v=([^&]*)/)) {
-        waitForButtonBar = setButtonBarInterval();
+    // Wait for DOM element function
+    // Credit: https://stackoverflow.com/a/61511955/10771609
+    function waitForElem(selector) {
+        return new Promise(resolve => {
+            if (document.querySelector(selector)) {
+                return resolve(document.querySelector(selector));
+            }
+    
+            const observer = new MutationObserver(mutations => {
+                if (document.querySelector(selector)) {
+                    resolve(document.querySelector(selector));
+                    observer.disconnect();
+                }
+            });
+    
+            observer.observe(document.body, {
+                childList: true,
+                subtree: true
+            });
+        });
     }
 
-    let waitForUrlChange = setInterval(() => {
-        if (location !== window.location.href && window.location.href.includes("watch?v=") && !waitForButtonBar) {
-            console.log("[YT-Unclutter] Video URL detected, uncluttering...");
-            waitForButtonBar = setButtonBarInterval();
-            location = window.location.href;
-        }
-    }, 1000);
+    function waitForAllElemChildrenToMatchSelector(element, selector) {
+        return new Promise(resolve => {
+            function detect() {
+                let children = [...element.children];
+                if (children.length > 0) {
+                    for (let i = 0; i < children.length; i++) {
+                        if (!children[i].querySelector(selector)) {
+                            return false;
+                        }
+                    }
+                    return true;
+                }
+                return false;
+            }
 
-    function doUnclutter() {
-        clearInterval(waitForButtonBar);
-        waitForButtonBar = null;
-        const ytButtonsParent = document.querySelector("#info>#menu-container>#menu>ytd-menu-renderer>#top-level-buttons-computed");
-        const ytButtons = [...ytButtonsParent.children];
+            if (detect()) {
+                resolve();
+            }
+    
+            const observer = new MutationObserver(mutations => {
+                if (detect()) {
+                    resolve();
+                    observer.disconnect();
+                }
+            });
+    
+            observer.observe(document.body, {
+                childList: true,
+                subtree: true
+            });
+        });
+    }
+
+    let titleElem = (await waitForElem("h1.title.ytd-video-primary-info-renderer>*"));
+    let title = '';
+    console.log("Title is: " + title);
+
+    let previousUrl = '';
+    const observer = new MutationObserver(async function (mutations) {
+        if (window.location.href !== previousUrl && title !== titleElem.innerText) {
+            console.log(`URL changed to ${window.location.href}`);
+            console.log(`Old title was ${title}, changed to ${titleElem.innerText}`);
+            title = titleElem.innerText;
+            previousUrl = window.location.href;
+            if (window.location.href.includes("watch?v=")) {
+                console.log("[YT-Unclutter] Video URL detected, uncluttering...");                
+                waitForElem("#top-level-buttons-computed path")
+                    .then(() => {
+                        doUnclutter();
+                    })
+
+            }
+        }
+    });
+    const config = { subtree: true, childList: true };
+    observer.observe(document, config);
+
+    async function doUnclutter() {
+        let ytButtonsParent = document.querySelector("#info>#menu-container>#menu>ytd-menu-renderer>#top-level-buttons-computed");
+        let ytButtons = [...ytButtonsParent.children];
+        await waitForAllElemChildrenToMatchSelector(ytButtonsParent, "path")
+        console.log("[YT-Unclutter] Detected path for all ytButtonsParent children");
 
         console.log(`[YT-Unclutter] Found ${ytButtons.length} native YT buttons`);
         console.log(ytButtons);
@@ -68,23 +132,22 @@
         }
 
         let buttonsToRemove = ["share", "download", "thank", "clip"];
-
-        let fakeIndex = 0;
+        console.log(`[YT-Unclutter] Removing ${ytButtons.length} buttons`);
         for (let i = 0; i < ytButtons.length; i++) {
             let button = ytButtons[i];
+            console.log(`Iter #${i} | Button: ${!!button}`)
             let buttonHash = djb2(button.querySelector("path").getAttribute("d"));
-            console.log(`Button #${fakeIndex} has hash: ${buttonHash}`);
+            console.log(`Button #${i} has hash: ${buttonHash}`);
             for (let buttonType of buttonsToRemove) {
                 if (buttonHash === buttonHashes[buttonType]) {
-                    console.log(`[YT-Unclutter] Removing ${buttonType} button (#${fakeIndex} - ${buttonHash})`);
+                    console.log(`[YT-Unclutter] Removing ${buttonType} button (#${i} - ${buttonHash})`);
                     button.remove();
                     break;
                 }
             }
-            fakeIndex++;
         }
 
-        //console.log(ytButtons);
+        console.log(ytButtons);
         console.log("[YT-Unclutter] Uncluttering done");
     }
 })();
